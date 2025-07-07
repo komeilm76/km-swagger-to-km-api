b4a636dbd0ecc867dd92fc89357742a735dc1b7b
komeilm76 <komeilmohammadian1376.web@gmail.com>
2025-07-07 15:53:22 +0330

    clear example

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,14 @@ import lib from './lib';
 const kmSwaggerToKmApi = lib;
 export default kmSwaggerToKmApi;
 
-const localPath = '/src/swagger.json';
-const urlPathPath = 'https://api.iqbaz.com/swagger/v1/swagger.json';
-const readPath = { localPath, urlPathPath };
-const writePath = '/test/lib/common/api/v2/config';
+// const localPath = '/src/swagger.json';
+// const urlPathPath = 'https://api.iqbaz.com/swagger/v1/swagger.json';
+// const readPath = { localPath, urlPathPath };
+// const writePath = '/test/lib/common/api/v2/config';
 
-kmSwaggerToKmApi.makeApiConfig(readPath.urlPathPath, writePath, {
-  pathScope: 'repository',
-  loadFromMainPath: false,
-  saveInCache: true,
-  exportMode: 'default',
-});
+// kmSwaggerToKmApi.makeApiConfig(readPath.urlPathPath, writePath, {
+//   pathScope: 'repository',
+//   loadFromMainPath: false,
+//   saveInCache: true,
+//   exportMode: 'default',
+// });
